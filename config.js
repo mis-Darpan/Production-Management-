7116b672b3b904a3bebcaaf32e9c1973bce735f2
mis-Darpan <mis@litpaxtechnology.com>
2026-06-29 14:36:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Yahan apni GAS Web App URL daalo
 // ============================================================
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyhrLcoUlgK-FKGEOquJaE0Td2Kx7sVQF3UP9frVRVwmH_RlmQNmE45hvyWlI6EbJw/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwR0hSGSrQTKlGJqDuUiufhC56pceDe00h3A2OPWbiXFVx2SUOHLWuVrKQxBDmD59UY/exec";
